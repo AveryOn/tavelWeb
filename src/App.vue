7e12d1d7b53d31f3149ae1f8e66dd45ac8417d7e
AveryOn <lnkonion2@gmail.com>
2023-05-22 02:03:17 +0300

    'middle'

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,22 @@
         <header class="header">header</header>
 
         <main class="main">
+
+            <!-- Первая Заглавная секция с параллакс эффектом -->
             <div class="main__block">
                 <div class="layer layer__front"></div>
                 <div class="layer layer__middle"></div>
-                <div class="layer layer__back"></div>
+                <h1 class="main__title">Unbelievable adventures!</h1>
             </div>
+            
+            <!-- Разделитель секций -->
+            
+            <!-- Вторая секция с контентом -->
+            <div class="main__basic">
+                <div class="separator-section"></div>
+
+            </div>
+
         </main>
 
         <footer class="footer">footer</footer>
@@ -17,11 +28,21 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+    document.addEventListener('scroll', (e) => {
+        document.body.style.cssText += `--scrollTop: ${window.scrollY}px`
+    
+    })
+})
+
 
 </script>
 
 <style>
 @import '~@csstools/normalize.css';
+@import url('https://fonts.googleapis.com/css2?family=Pangolin&display=swap');
 
 :root {
     --sync: calc(1vw + 1vh);
@@ -69,15 +90,26 @@ body {
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-direction: column;
     /* border: var(--border); */
 }
 
 .main__block {
     width: 100%;
     min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     background-image: url(./images/background-main.jpg);
     background-size: cover;
     background-position: center;
+}
+.main__title{
+    position: relative;
+    top: 50px;
+    font-family: 'Pangolin', cursive;
+    text-shadow: 1px 1px 20px rgba(0, 0, 0, 0.449);
+    font-size: 4em;
 }
 .layer{
     position: absolute;
@@ -88,17 +120,40 @@ body {
 }
 .layer__front{
     background-image: url(./images/main--front.png);
+    transform: translate3d(0, calc((var(--scrollTop) / -14)), 0);
     z-index: 5;
+    background-size: cover;
+    background-position: center;
 }
 .layer__middle{
     background-image: url(./images/main--middle.png);
+    transform: translate3d(0, calc((var(--scrollTop) / -12)), 0);
     z-index: 4;
+    background-size: cover;
+    background-position: center;
 }
-/* .layer__back{
-    background-image: url(./images/main--back.png);
-    z-index: 3;
-} */
-
+.separator-section{
+    position: absolute;
+    top: -10px;
+    width: 100%;
+    height: 150px;
+    background-image: url(./images/separator.png);
+    transform: translate3d(0, calc((var(--scrollTop) / -26)), 0);
+    z-index: 2;
+    background-size: cover;
+    background-position: center;
+}
+.main__basic{
+    position: relative;
+    top: 10px;
+    width: 100%;
+    height: 100vh;
+    z-index: 5;
+    background-image: url(./images/background-basic.jpg);
+    transform: translate3d(0, calc((var(--scrollTop) / -14)), 0);
+    background-size: cover;
+    background-position: center;
+}
 .footer {
     width: 100%;
     display: flex;
